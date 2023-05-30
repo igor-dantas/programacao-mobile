d230cb30ae0cd02e53dcd54388232f846a90fa76
Igor Dantas Quintanilha <55963347+igor-dantas@users.noreply.github.com>
2023-05-30 19:44:43 +0000

    feat: tipos de funções

---
+++ tiposdefuncoes.js
@@ -0,0 +1,16 @@
+//vários formatos de funções
+//forma literal
+function somar(x, y){
+    return x+y;
+}
+
+//armazenar em uma função
+let = function(x, y){
+    return x+y;
+}
+
+//arrow function
+let somar = (x, y) => x + y;
+
+//chamando funções
+console.log(somar(10, 5));
