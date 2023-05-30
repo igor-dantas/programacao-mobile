b5a4a042d07d934cceb463903efba04664a8c321
Igor Dantas Quintanilha <55963347+igor-dantas@users.noreply.github.com>
2023-05-30 20:14:18 +0000

    feat: primeiro app

---
+++ projetos/primerioapp/PrimeiroApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function PrimeiroApp(){
+  return (
+    <View>
+      <Text>Olá mundo!!!</Text>
+      <Text>Meu primeiro app</Text>
+    </View>
+  );
+}
