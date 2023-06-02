7369291516d96b1919ee3a6e564b4fee1e7fc6f1
Igor Dantas Quintanilha <55963347+igor-dantas@users.noreply.github.com>
2023-06-02 18:40:01 +0000

    feat

--- projetos/segundoapp/App.js
+++ projetos/segundoapp/App.js
@@ -3,8 +3,10 @@ import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View  style={styles.pagina}>
-      <Text>Olá mundo!</Text>
+    <View style={styles.centralizador}>
+      <View  style={styles.pagina}>
+        <Text style={styles.texto}>Olá mundo!</Text>
+      </View>
     </View>
   );
 }
@@ -13,10 +15,22 @@ export default function App() {
 //criando estilo com StyleSheet
 const styles = StyleSheet.create({
   pagina: {
+    flexDirection: 'row',
     width: 200,
     height: 200,
     backgroundColor: '#FF0000',
     justifyContent: 'center',
     alignItems: 'center',
   },
+  texto: {
+    color: '#FFFFFF',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  centralizador: { 
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+  }
 });
